9f5fc48276130301367fb22d1a4c554c8992019e
Bhavya Reddy <bhavyareddyrpt2406@gmail.com>
2026-06-09 11:04:28 +0530

    Added banner grabbing and PDF report download feature

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+from flask import Flask, render_template, request, send_file
+from reportlab.pdfgen import canvas
 from flask import Flask, render_template, request
 import socket
+
 
 app = Flask(__name__)
 
@@ -17,29 +20,30 @@ PORTS = {
     3306: "MySQL",
     3389: "RDP"
 }
-def grab_banner(ip, port):
 
-    try:
-        s = socket.socket()
-        s.settimeout(2)
-
-        s.connect((ip, port))
-
-        if port == 80:
-            s.send(b"HEAD / HTTP/1.0\r\n\r\n")
-
-        banner = s.recv(1024).decode(errors="ignore")
-
-        s.close()
-
-        return banner.split("\n")[0]
-
-    except:
-        return "Banner not available"
 RISKY_PORTS = {
     21: "FTP",
     23: "Telnet"
 }
+
+
+def grab_banner(ip, port):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(2)
+
+        sock.connect((ip, port))
+
+        try:
+            banner = sock.recv(1024).decode(errors="ignore").strip()
+        except:
+            banner = "Banner not available"
+
+        sock.close()
+        return banner
+
+    except:
+        return "Banner not available"
 
 
 def scan_target(target):
@@ -71,8 +75,6 @@ def scan_target(target):
         if len(results) == 0:
             results.append("-- No open ports found")
 
-        
-
         if risk_score == 0:
             results.append("Risk Level: LOW")
         elif risk_score == 1:
@@ -80,8 +82,8 @@ def scan_target(target):
         else:
             results.append("Risk Level: HIGH")
 
-    except:
-        results.append("Invalid IP Address or Hostname")
+    except Exception as e:
+        results.append(f"Error: {str(e)}")
 
     return results
 
@@ -97,6 +99,7 @@ def home():
         target = request.form["target"]
 
         results = scan_target(target)
+        generate_pdf(results)
 
         with open("scan_history.txt", "a") as file:
             file.write(target + "\n")
@@ -113,7 +116,26 @@ def home():
         results=results,
         history=history
     )
+def generate_pdf(results):
+    pdf = canvas.Canvas("scan_report.pdf")
 
+    y = 800
+    pdf.drawString(100, y, "NetRecon-X Scan Report")
+
+    y -= 40
+
+    for result in results:
+        pdf.drawString(100, y, result)
+        y -= 20
+
+    pdf.save()
+
+@app.route("/download")
+def download():
+    return send_file(
+        "scan_report.pdf",
+        as_attachment=True
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
